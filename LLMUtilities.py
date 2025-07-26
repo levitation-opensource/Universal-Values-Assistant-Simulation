eb5b490673b111f0e8c803428373f868da49d836
Roland Pihlakas <roland@simplify.ee>
2025-07-26 22:44:29 +0300

    Reducing max token count of the local model

--- LLMUtilities.py
+++ LLMUtilities.py
@@ -349,7 +349,7 @@ def get_max_tokens_for_model(model_name):
   
   if is_local:
 
-    return 75000    # TODO: read from config
+    return 64000    # TODO: read from config
 
   elif is_claude:
        
